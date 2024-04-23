00b7de3fd4f5cebf2f2928cbed81db10c9fb9060
miu200521358 <garnet200521358@gmail.com>
2024-04-23 22:58:19 +0900

    python用ディレクトリに名前変換

--- estimate.sh
+++ estimate.sh
@@ -8,6 +8,6 @@ for name in ${names[@]}; do
     echo "=================================="
     echo $name
     # 姿勢推定+追跡+json出力
-    python src/exec_track.py video.source=inputs/$name.mp4 video.output_dir=outputs
+    python py/exec_track.py video.source=inputs/$name.mp4 video.output_dir=outputs
 done
 
