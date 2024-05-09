9e404f5a6724dc4497e3a85f082b4129bc8bfc3c
miu <38824651+miu200521358@users.noreply.github.com>
2024-05-09 17:53:45 +0900

    Update mat4-colab.sh

--- mat4-colab.sh
+++ mat4-colab.sh
@@ -1,14 +1,14 @@
 echo "4D-Humans -----------------------------"
-python py/exec_track.py video.source "$input_video_path" video.output_dir "$process_img_dir_path"
+python py/exec_track.py video.source "$input_video_path" video.output_dir "$process_dir_path"
 
 echo "pkl to json -----------------------------"
-python py/convert_pkl2json.py "$process_img_dir_path"
+python py/convert_pkl2json.py "$process_dir_path"
 
 echo "mediapipe -----------------------------"
-python py/exec_mediapipe.py --video "$input_video_path" --output_dir "$process_img_dir_path"
+python py/exec_mediapipe.py --video "$input_video_path" --output_dir "$process_dir_path"
 
 echo "smooth -----------------------------"
-python py/smooth.py "$process_img_dir_path"
+python py/smooth.py "$process_dir_path"
 
 echo "convert motion -----------------------------"
-./build/mat4 -modelPath="$model_path" -dirPath="$process_img_dir_path"
+./build/mat4 -modelPath="$model_path" -dirPath="$process_dir_path"
